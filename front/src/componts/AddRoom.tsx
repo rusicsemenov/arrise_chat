4a2e792f6ddbf6feb208a8462925bb5525b8ffd8
Rusic <rusic@fastmail.com>
2025-11-08 00:07:11 +0100

    refactor AddRoom component to use createPortal for modal rendering; improve modal accessibility

--- front/src/componts/AddRoom.tsx
+++ front/src/componts/AddRoom.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useEffect, useRef, useState } from 'react';
 import { useSocketContext } from './SocketProvider.tsx';
 import { toast } from 'react-toastify';
+import { createPortal } from 'react-dom';
 
 const AddRoom = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -45,33 +46,36 @@ const AddRoom = () => {
             <button className="btn ml-auto" onClick={() => setIsOpen(true)}>
                 Create New Room
             </button>
-            {isOpen && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Create New Room</h2>
-                        <form onSubmit={onFormSubmit} className="flex-column gap-2">
-                            <label htmlFor="room-name">Room Name</label>
-                            <input type="text" id="room-name" required ref={nameInputRef} />
+            {isOpen && typeof document !== 'undefined'
+                ? createPortal(
+                      <div className="modal">
+                          <div className="modal-content">
+                              <h2>Create New Room</h2>
+                              <form onSubmit={onFormSubmit} className="flex-column gap-2">
+                                  <label htmlFor="room-name">Room Name</label>
+                                  <input type="text" id="room-name" required ref={nameInputRef} />
 
-                            <label htmlFor="room-description">Room Description</label>
-                            <textarea id="room-description"></textarea>
+                                  <label htmlFor="room-description">Room Description</label>
+                                  <textarea id="room-description"></textarea>
 
-                            <div className="flex gap-2 justify-center">
-                                <button type="submit" className="btn w100">
-                                    Create
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn secondary"
-                                    onClick={() => setIsOpen(false)}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            )}
+                                  <div className="flex gap-2 justify-center">
+                                      <button type="submit" className="btn w100">
+                                          Create
+                                      </button>
+                                      <button
+                                          type="button"
+                                          className="btn secondary"
+                                          onClick={() => setIsOpen(false)}
+                                      >
+                                          Cancel
+                                      </button>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>,
+                      document.body,
+                  )
+                : null}
         </>
     );
 };
